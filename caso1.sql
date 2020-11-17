8201cdb5c1d9c9175023540e34409848e424efb6
ivncastillo <ivncastillo@gmail.com>
2020-11-17 20:55:57 -0300

    primera

---
+++ caso1.sql
@@ -0,0 +1,28 @@
+
+
+CREATE DATABASE caso1;
+
+
+CREATE TABLE trabajador(
+rut INT
+,nombre VARCHAR(25)
+,direccion VARCHAR(255)
+,id_dpto INT
+, PRIMARY KEY (rut)
+, FOREIGN KEY (id_dpto) REFERENCES departamento(id_dpto)
+);
+
+
+CREATE TABLE departamento(
+id_dpto INT
+,nombre VARCHAR(25)
+, PRIMARY KEY (id_dpto)
+);
+
+CREATE TABLE liquidacion(
+id_liquidacion INT
+,drive VARCHAR(255)
+,rut INT
+, PRIMARY KEY (id_liquidacion)
+, FOREIGN KEY (rut) REFERENCES trabajador(rut)
+);
---
+++ caso2.sql
@@ -0,0 +1,34 @@
+
+
+CREATE DATABASE caso2;
+
+
+CREATE TABLE alumno(
+rut INT
+,nombre VARCHAR(25)
+,id_curso INT
+, PRIMARY KEY (rut)
+, FOREIGN KEY (id_curso) REFERENCES curso(id_curso)
+);
+
+
+CREATE TABLE curso(
+id_curso INT
+,nombre VARCHAR(25)
+, PRIMARY KEY (id_curso)
+);
+
+CREATE TABLE prueba(
+id_prueba INT
+,puntaje INT
+,id_profesor INT
+, PRIMARY KEY (id_prueba)
+, FOREIGN KEY (id_profesor) REFERENCES profesor(id_profesor)
+);
+
+CREATE TABLE profesor(
+id_profesor INT
+,nombre VARCHAR(50)
+,departamento VARCHAR(50)
+, PRIMARY KEY (id_profesor)
+);
